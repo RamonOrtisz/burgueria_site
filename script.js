1e4b274d6e117362ebd733ad377cc8e7357b8137
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-02-06 15:18:35 -0300

    adicionando valor total do carrinho

--- script.js
+++ script.js
@@ -81,25 +81,35 @@ function updateCartModal() {
 
     cart.forEach(item => {
         const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
 
         cartItemElement.innerHTML = `
-        <div>
+        <div class="flex items-center justify-between">
             <div>
-                <p>${item.name}</p>
-                <p>${item.quantity}</p>
-                <p>R$ ${item.price}</p>
+                <p class="font-medium">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
             </div>
-        </div>
+        
 
-        <div>
-         <button>
-            remover
-         </button>
+            
+            <button>
+                remover
+            </button>
+            
+
         </div>
 
         `
 
+        total += item.price * item.quantity;
+
         cartItemsContainer.appendChild(cartItemElement)
     })
+
+    cartTotal.textContent = total.toLocaleString("pr-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
 
 }
