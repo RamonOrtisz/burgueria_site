28fe36ea3ce30852b55fc1044051986d203a89e5
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-02-07 16:06:12 -0300

    add remove/add items

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function updateCartModal() {
                 <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
             </div>
 
-            <button class="remove-from-cart-btn">
+            <button class="remove-from-cart-btn" data-name="${item.name}">
                 remover
             </button>
 
@@ -114,3 +114,70 @@ function updateCartModal() {
 }
 
 // funcao para remover o item do carrinho
+
+cartItemsContainer.addEventListener("click", function (event) {
+    if(event.target.classList.contains("remove-from-cart-btn")) {
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1) {
+        const item = cart[index];
+       
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(event) {
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+    // 
+})
+
+checkoutBtn.addEventListener("click", function() {
+    if(cart.length === 0) return;
+
+    if(addressInput.value === "") {
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+})
+
+// verificar a hora e manipular o card horario
+function checkRestOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestOpen();
+
+if(isOpen) {
+    spanItem.classList.remove("gb-red-500");
+    spanItem.classList.add("gb-green-600");
+
+} else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
